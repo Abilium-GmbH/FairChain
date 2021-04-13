30f33d9f936c33fe1ea388a775264b2efbee50d6
Marco De Liso <marco.deliso@students.unibe.ch>
2021-04-13 13:56:50 +0200

    Added Implementation of colors in Nodes/fixed bug node color change+edge together/added arrow to edges/removed inheritance of edges from nodes/hided data for experimenting

--- src/app/components/fairChain/fairChain.component.ts
+++ src/app/components/fairChain/fairChain.component.ts
@@ -2,9 +2,12 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { MatLabel } from '@angular/material/form-field';
 import { fromEvent, Subscription } from 'rxjs';
 import * as vis from 'vis-network';
+import {colors} from '@angular/cli/utilities/color';
+import set = Reflect.set;
+import {parseColor} from 'vis-util';
 
 enum Changing {
-  NodeLabel, NodeColor,
+  NodeLabel, EdgeLabel, NodeColor,
   None
 }
 
@@ -24,13 +27,13 @@ enum Changing {
 
 export class FairChainComponent implements OnInit {
 
-  public isChangeNodeColor=false;
+  public isChangeNodeColor = false;
   public isAddingNodes = false;
   public isAddingEdges = false;
   public isDeletingNodesOrEdges = false;
   public nodeLabel = "";
   public edgeLabel = "";
-  public nodeColor ="#002AFF";
+  public nodeColor = "#002AFF";
   public isChangeNodeLabel = false;
   public isChangeEdgeLabel = false;
   public isShowNodeOptions = false;
@@ -45,6 +48,50 @@ export class FairChainComponent implements OnInit {
   // create an array with edges
   private edges: vis.Edge[] = [];
 
+/*
+  private edgeSamples = new vis.DataSet([
+    {
+    id: 1,
+    label: 'Green',
+    color: {
+      border: 'green',
+      background: 'green'
+    }
+  }, {
+    id: 2,
+    label: 'Red',
+    color: {
+      border: 'red',
+      background: 'red'
+    }
+  }]);
+ */
+
+  /*
+  private edgeOptions: {
+    edges: {
+      hoverWidth: function(width) {
+      return width+1;
+      }
+    }
+  };
+
+   */
+
+/*
+  //
+  private edgeEditColor: vis.Color[] = [
+    { border: 'green', background: 'green' },
+    { border: 'light red', background: 'red' },
+    { border: 'black', background: 'black' }
+  ];
+
+
+ */
+
+
+
+
   // create a network
   private data: vis.Data = {
     nodes: this.nodes,
@@ -58,8 +105,17 @@ export class FairChainComponent implements OnInit {
       physics: true
     },
     edges: {
+      color: {
+        inherit: false
+      },
       smooth: true,
-      physics: true
+      physics: true,
+      arrows: {
+        to: {
+         enabled: false,
+        }
+      },
+      dashes: true
     },
     manipulation: {
       // defines logic for Add Node functionality
@@ -79,36 +135,33 @@ export class FairChainComponent implements OnInit {
         }
       },
       // Responsible for the Edit Node Label
-      editNode: (data, callback) => {
-        data.label = this.nodeLabel;
-        callback(data);
-      },
-      editEdge: (data, callback) => {
-        data.label = this.nodeLabel;
-        callback(data);
-
-        switch(+this.changes){
+      editNode: (nodeData, callback) => {
+        switch (+this.changes) {
           case Changing.NodeLabel:{
-            data.label = this.nodeLabel;
-            this.nodes=this.nodes.filter(node=> node.id!=data.id);
-            this.nodes.push(data)
-            break
+            nodeData.label = this.nodeLabel;
+            break;
           }
           case Changing.NodeColor:{
-            data.color=this.nodeColor; break
+            nodeData.color=this.nodeColor;
+            break;
           }
         }
-
+        callback(nodeData);
         this.nodes.forEach(node => {
-          if (node.id==data.id) node=data
+          if (node.id==nodeData.id) node=nodeData
         })
-        callback(data)
+      },
+      editEdge: (data, callback) => {
+      /*
+        data.color = 'red';
+      */
+        data.label = this.nodeLabel;
+        callback(data);
       }
     },
     groups: {
       myGroup: {color:{background:'red'}, borderWidth:3}
     }
-
   };
 
   constructor() { }
@@ -165,27 +218,52 @@ export class FairChainComponent implements OnInit {
   // Defines actions when user clicks on Nodes or Edges
   private onClick(params) {
     if (params.nodes && params.nodes.length >= 1) {
-      if (this.isChangeNodeLabel) {
-        this.network.editNode();
-      }
+      if (this.changes==Changing.NodeLabel) this.network.editNode();
+      if (this.changes==Changing.NodeColor) this.network.editNode();
     }
     if (params.edges && params.edges.length >= 1 && params.nodes.length == 0) {
       if (this.isChangeEdgeLabel) {
         this.network.editEdgeMode();
       }
-      if (this.changes==Changing.NodeLabel) this.network.editNode();
-      if (this.changes==Changing.NodeColor) this.network.editNode();
     }
+
+
+
   }
+    /*
+    if (this.onClick.length = 2) {
+      this.network.deleteSelected();
+    }
+
+     */
+
 
   // Boolean switch value if someone wants to change the nodeLabel name for button color
   public changeNodeName() {
+    if (this.changes == Changing.NodeColor) {
+      this.isChangeNodeColor = false;
+    }
     this.isChangeNodeLabel = !this.isChangeNodeLabel;
     this.isChangeEdgeLabel = !this.isChangeEdgeLabel;
     this.changes=Changing.NodeLabel
     if(!this.isChangeNodeLabel) this.changes=Changing.None
   };
 
+/*
+  public nodeID = params.nodes['nodes']['0'];
+  if (nodeID) {
+    var clickedNode = this.nodes.find(nodeID);
+    clickedNode.color = {
+      border: '#000000',
+      background: '#000000',
+      highlight: {
+        border: '#2B7CE9',
+        background: '#D2E5FF'
+      }
+    }
+    this.nodes.push(clickedNode);
+  }
+*/
 
   // initialize network properties
   private get graph(): HTMLElement {
@@ -193,9 +271,12 @@ export class FairChainComponent implements OnInit {
   }
 
   public changeColor(){
-    this.isChangeNodeColor=!this.isChangeNodeColor;
-    this.changes=Changing.NodeColor;
-    if(!this.isChangeNodeColor) this.changes=Changing.None
+    if (this.changes == Changing.NodeLabel) {
+      this.isChangeNodeLabel = false;
+    }
+    this.isChangeNodeColor =! this.isChangeNodeColor;
+    this.changes = Changing.NodeColor;
+    if(!this.isChangeNodeColor) this.changes = Changing.None
   }
 }
 
