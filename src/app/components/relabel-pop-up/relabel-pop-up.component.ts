7924ef2085962d4815b4101c9b542f241f3bdc10
Jethro Warnett <jethro.warnett@students.unibe.ch>
2021-04-30 15:25:35 +0200

    Started again relabel constraints
    There is a maximal colum width and a maximal row height.
    There is a maximal character limit for a label.

--- src/app/components/relabel-pop-up/relabel-pop-up.component.ts
+++ src/app/components/relabel-pop-up/relabel-pop-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
+import { strict as assert } from 'assert';
 import { relabelPopUpInfo } from '../interfaces';
 
 @Component({
@@ -7,6 +8,13 @@ import { relabelPopUpInfo } from '../interfaces';
   styleUrls: ['./relabel-pop-up.component.scss']
 })
 export class RelabelPopUpComponent implements OnInit {
+
+  private minCol: number = 10;
+  private maxCol: number = 30;
+  private minRow: number = 1;
+  private maxRow: number = 5;
+  private maxNumCharacters: number = 144;
+  private prevLabel: string;
 
   constructor() { }
 
@@ -19,14 +27,33 @@ export class RelabelPopUpComponent implements OnInit {
   @ViewChild('relabelTextArea') relabelTextAreaRef: ElementRef;
 
   ngOnInit(): void {
+    assert(this.minCol <= this.maxCol);
+    assert(this.minRow <= this.maxRow);
   }
 
   ngAfterViewInit(): void {
     this.relabelTextArea.innerText = this.label;
+    this.prevLabel = this.label;
   }
 
   public updateLabel() {
+    if (this.isNotAbleToUpdate()) this.denyUpdate();
+    this.applyUpdateToLabel()
+  }
+  
+  private denyUpdate() {
+    this.relabelText = this.prevLabel;
+    throw new Error('Character Limit reached');
+  }
+
+  private applyUpdateToLabel() {
+    this.prevLabel = this.relabelText;
     this.labelChange.emit(this.convertToMultiline(this.relabelTextArea.innerText));
+  }
+
+  private isNotAbleToUpdate(): boolean {
+    if (this.relabelText.length > this.maxNumCharacters) return true;
+    if (this.relabelText.split('\n').length > this.maxRow + 1) return true;
   }
 
   private convertToMultiline(text: string) {
@@ -35,6 +62,14 @@ export class RelabelPopUpComponent implements OnInit {
 
   public get relabelTextArea() {
     return this.relabelTextAreaRef.nativeElement;
-  } 
+  }
+  
+  public get relabelText(): string {
+    return this.relabelTextArea.innerText;
+  }
+
+  public set relabelText(text: string) {
+    this.relabelTextArea.innerText = text;
+  }
   
 }
