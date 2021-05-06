1a2d1f99619eead47f543c88ea30d865af7ef2eb
Lawrence Chiang <lesorax@gmail.com>
2021-05-06 22:50:54 +0200

    Revert "Merge branch 'main' into dev"
    This reverts commit 6a306fdf89f4b0d2fd15722c5c5923cde0289a48, reversing
    changes made to 295e33411eec6270fa4ba3837e0690d2752a34e9.

---
+++ src/app/undoRedo.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { DataSetNodes, DataSetEdges} from "vis-network/peer/esm/vis-network";
+import { ImportExportService } from './importExport.service';
+
+@Injectable({
+    providedIn: 'root'
+  })
+
+export class UndoRedoService{
+    private snapshots=[];
+    private counter = 0;
+
+    constructor(private importExportService:ImportExportService) {
+        this.snapshots.push("{\"nodes\":[],\"edges\":[]}");
+    }
+
+    public getPredecessorSnapshot(){
+        if (this.counter > 0) this.counter--;
+        return JSON.parse(this.snapshots[this.counter]);
+    }
+
+    public getSuccessorSnapshot(){
+        if (this.counter < this.snapshots.length - 1) this.counter++;
+        return JSON.parse(this.snapshots[this.counter]);
+    }
+
+    public addSnapshot(nodes: DataSetNodes, edges: DataSetEdges){
+        if (this.counter + 1 < this.snapshots.length) this.snapshots.splice(this.counter + 1, this.snapshots.length);
+        this.counter++;
+        this.snapshots[this.counter] = this.importExportService.convertNetworkToJSON(nodes, edges);
+    }
+}
