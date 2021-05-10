e3e3b5a961c286226dc5d90b1cabf49d7bbc2a7a
Jethro Warnett <jethro.warnett@students.unibe.ch>
2021-05-10 15:21:46 +0200

    Added Test Script
    After the Zühlke discussion, a test script is provided for the dynamic pop up component to be tested by hand.

--- src/app/components/relabel-pop-up/relabel-pop-up.component.spec.ts
+++ src/app/components/relabel-pop-up/relabel-pop-up.component.spec.ts
@@ -1,8 +1,13 @@
+/*
+* After consulatation with the Zühlke Firm, this component will not be tested.
+* The work required to test it, is far to expensive. Instead a script
+* of scenarios to be tested by hand is provided in the same folder.
+*/
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RelabelPopUpComponent } from './relabel-pop-up.component';
 
-/*
 describe('RelabelPopUpComponent', () => {
   let component: RelabelPopUpComponent;
   let fixture: ComponentFixture<RelabelPopUpComponent>;
@@ -19,9 +24,4 @@ describe('RelabelPopUpComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(true).toBeTrue();
-  });
 });
-*/
